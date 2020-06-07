28a689ba7c6cc925fe058b794bd7b51a91ed1c34
Jing Liang <jing.liang@scuvis.org>
2020-06-07 10:39:41 +0800

    feat(#8 test): not update backstage pass product quality when quality is 50

--- src/test/java/com/tdd/gilederose/ProductWarehouseTest.java
+++ src/test/java/com/tdd/gilederose/ProductWarehouseTest.java
@@ -95,6 +95,20 @@ public class ProductWarehouseTest {
     }
 
     @Test
+    public void should_not_update_backstage_pass_product_quality_when_quality_is_50(){
+        Product product = new BackstagePassProduct(2L,"backstage pass product",4, 50);
+        ProductWarehouse productWarehouse = new ProductWarehouse();
+        productWarehouse.addProduct(product);
+
+        List<Product> productList = productWarehouse.getProductList();
+        int stockId = productWarehouse.getStockId(product.getId());
+        productWarehouse.updateStockInfo(stockId);
+
+        assertThat(productList.get(stockId).getSellIn(), is(3));
+        assertThat(productList.get(stockId).getQuality(), is(50));
+    }
+
+    @Test
     public void should_update_backstage_pass_product_quality_to_0_successfully(){
         Product product = new BackstagePassProduct(2L,"backstage pass product",-1, 10);
         ProductWarehouse productWarehouse = new ProductWarehouse();
